5e368411f73e5c3e3a784160eab4c700335af3cb
Jonas Camillus Jeppesen <jonascj@jonascj.dk>
2021-08-13 13:44:17 +0200

    Handle numbering of multi-line strings and doc strings correctly, added linenostart and linenostep options.

--- pygments/formatters/rtf.py
+++ pygments/formatters/rtf.py
@@ -50,6 +50,12 @@ class RtfFormatter(Formatter):
         Font size for line numbers. Size is specified in half points. The
         default is 18 half-points, giving a 9pt font.
 
+    `linenostart`
+        The line number for the first line (default: ``1``).
+
+    `linenostep`
+        If set to a number n > 1, only every nth line number is printed.
+
         .. versionadded:: 2.0
     """
     name = 'RTF'
@@ -73,6 +79,8 @@ class RtfFormatter(Formatter):
         self.fontsize = get_int_opt(options, 'fontsize', 0)
         self.linenos = options.get('linenos', False)
         self.lineno_fontsize = get_int_opt(options, 'lineno_fontsize', 18)
+        self.linenostart = abs(get_int_opt(options, 'linenostart', 1))
+        self.linenostep = abs(get_int_opt(options, 'linenostep', 1))
 
     def _escape(self, text):
         return text.replace('\\', '\\\\') \
@@ -131,23 +139,31 @@ class RtfFormatter(Formatter):
         ## line numbering setup
         if self.linenos:
             
-            # first pass of tokens to obtain total number of lines 
+            # first pass of tokens (tokensource) to obtain total number of lines 
+            # and deal with tokens of type Literal.String.Doc
             num_of_lines = 0
             tokens = []
             for ttype, value in tokensource:
 
                 # tokensource is a generator, so make a copy
-                tokens.append((ttype,value))
-                if value == u'\n':
-                    #print("+1 ttype", ttype, "value", repr(value))
+                if ttype == Token.Literal.String.Doc:
+                    lines = value.split('\n')
+                    for line in lines:
+                        num_of_lines += 1
+                        tokens.append((ttype, line+'\n'))
+                    
+                elif value == u'\n':
                     num_of_lines += 1
+                    tokens.append((ttype, value))
+                else:
+                    tokens.append((ttype, value))
+                    
 
-            #print("num_of_lines", num_of_lines)
             # width of (space) padded line number string
             linenos_width = len(str(num_of_lines))            
 
             # line number output book keeping
-            lineno = 1 
+            lineno = self.linenostart
             output_lineno = True
 
         else:
@@ -163,14 +179,19 @@ class RtfFormatter(Formatter):
 
             # line numbers
             if self.linenos:                                 
-                #print("ttype", ttype, "value", repr(value))
+
                 if output_lineno:                                      
-                    lineno_str = str(lineno).rjust(linenos_width)      
+
+                    if (lineno-self.linenostep)%self.linenostep == 0:
+                        lineno_str = str(lineno).rjust(linenos_width)      
+                    else:
+                        lineno_str = "".rjust(linenos_width)      
+
                     outfile.write(u'{\\fs%d \\cf1 %s  }'\
                         % (self.lineno_fontsize, lineno_str))          
                     output_lineno = False                              
                                                                        
-                if ttype == Token.Text and value == u'\n':             
+                if value.endswith('\n'):
                     output_lineno = True                               
                     lineno += 1                                        
 
@@ -188,8 +209,10 @@ class RtfFormatter(Formatter):
                 buf.append('\\chbrdr\\chcfpat%d' %
                            color_mapping[style['border']])
             start = ''.join(buf)
+
             if start:
                 outfile.write('{%s ' % start)
+
             outfile.write(self._escape_text(value))
             if start:
                 outfile.write('}')
